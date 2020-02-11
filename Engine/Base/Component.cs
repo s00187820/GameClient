0a6ad443cc9da1da6c9c59ba17d622eb62df4b04
Cian O Reilly <S00187820@mail.itsligo.ie>
2020-02-11 17:38:11 +0000

    Component Methods

---
+++ Engine/Base/Component.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Base
+{
+    //need to be public to call them in the gameclient
+    public class Component
+    {
+        public string ID { get; set; }
+        public bool Enabled { get; set; }
+
+        public Component()
+        {
+            ID = GetType().Name + Guid.NewGuid();
+            Enabled = true;
+        }
+
+
+        public virtual void intialize() { }
+        public virtual void intialized() { }
+        public virtual void Update() { }
+
+        public event ObjectIDHandler OnDestroy;
+
+        public virtual void Destroy()
+        {
+            if (OnDestroy != null)
+                OnDestroy(ID);
+        }
+
+    }
+}
